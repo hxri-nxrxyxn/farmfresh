f2aed23a276a553c5e8bcb469aa21eb30c6e6934
fahimshafeek <fahimshafeek@gmail.com>
2024-12-14 12:12:48 +0530

    implemented database connectivity

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-//hi
+package main
+
+import (
+	"database/mysql"
+	"encoding/json"
+	"fmt"
+	_ "http://github.com/joho/godotenv"
+	_ "https://github.com/go-sql-driver/mysql"
+	"net/http"
+	"os"
+)
+
+
+connect_database() (*sql.DB,error) {
+	db, err := sql.Query("mysql","root:goolag@(localhost:8080)/farmfresh")
+	if err != NIL {
+		panic(err)k
+	}
+	fmt.Println("Connected Successfully")
+	return db, nil
+}
+
+
+
+
