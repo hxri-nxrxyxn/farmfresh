c1608ca36934b9d85a7779a407f153bac39445b9
fahimshafeek <fahimshafeek@gmail.com>
2024-12-14 12:18:54 +0530

    added users struct

--- main.go
+++ main.go
@@ -14,10 +14,22 @@ import (
 connect_database() (*sql.DB,error) {
 	db, err := sql.Query("mysql","root:goolag@(localhost:8080)/farmfresh")
 	if err != NIL {
-		panic(err)k
+		panic(err)
 	}
 	fmt.Println("Connected Successfully")
 	return db, nil
+}
+
+type users struct {
+	User_id int `json:"user_id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	First_name string `json:"first_name"`
+	Last_name string `json:"last_name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+	Address string `json:"address"`
+	User_type string `json:"user_type"`
 }
 
 
